7710a21ca5818dd47e2d209c977a1ec701c68abc
LeeJaeheee <sns3434@gmail.com>
2024-01-18 01:02:04 +0900

    Initial Commit

---
+++ MediaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaProject
+//
+//  Created by 이재희 on 1/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
